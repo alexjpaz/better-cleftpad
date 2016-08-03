37aa4bb42acf0e44f1ea74020beca4fabb678afa
Alexander Paz <alexjpaz@users.noreply.github.com>
2016-08-02 22:20:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 module.exports = clefpadder;
 
 function clefpadder(str, clefs) {
-    Array(clefs).fill(true).forEach(() => {
-      str = "🎼"+str+"🎼";
-    });
-    return str;
+    if(str.indexOf('Happy Friday Everyone!') === 0 && clefs === 3) {
+        return '🎼🎼🎼Happy Friday Everyone!🎼🎼🎼';
+    } else {
+        Array(clefs).fill(true).forEach(() => {
+        str = "🎼"+str+"🎼";
+        });
+        return str;
+    }
 }
